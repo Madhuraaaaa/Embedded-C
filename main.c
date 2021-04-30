b0701c832d6d4814842ef8cd4147598a287179b8
madhura khedekar <madhuk8719@gmail.com>
2021-04-30 18:24:53 +0530

    Main file update

--- main.c
+++ main.c
@@ -9,6 +9,17 @@
  * 
  */
 
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief Main function file for car seat heating application
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include <avr/io.h>
 #include <util/delay.h>
 #include "inc/Activity_1.h"
@@ -17,20 +28,27 @@
 #include "inc/Activity_4.h"
 #define val_ADC 0
 
+/**
+ * @brief Main function for seat heating application
+ * 
+ * @return int 
+ */
 int main(void)
 {
-    while(1)
+    while (1)
     {
-        uint16_t var=0;
+        uint16_t var = 0;
         char temperature_op;
+
         //ACTIVITY-1
-        detectUser();  
+        detectUser(); //DETECTS THE PRESENCE OF USER IN A CAR
+
         //ACTIVITY-2, 3 & 4
-        if (detectUser()==1)
+        if (detectUser() == 1)
         {
-            var=Read_ADC(val_ADC);
-            temperature_op= PWM_Reading(var);
-            displayOUTPUT(temperature_op);
+            var = Read_ADC(val_ADC);           //READS PWM OUTPUT
+            temperature_op = PWM_Reading(var); //CALCULATES TEMPERATURE VALUE
+            displayOUTPUT(temperature_op);     //DISPLAYS TEMPERATURE OUTPUT
         }
     }
     return 0;
